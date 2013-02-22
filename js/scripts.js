68ee8c68a202063f6239017ed86379085e4858cc
Roy Bidwell <rbidwell@gmail.com>
2013-02-22 10:36:16 -0800

    First Upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  
+  // Good stuff
+	$('div#lastfm').lastFM({
+		username: 'ewuweblab',
+		apikey: 'bdafa5aeb85d3c787dd6c9c2f91fc872',
+<<<<<<< HEAD
+		number: 3,
+		artSize: 'large',
+		onComplete: function(){
+			//Done
+			
+=======
+		number: 4,
+		artSize: 'large',
+		onComplete: function(){
+			//Done
+		
+>>>>>>> First Upload
+		}
+	});
+
+
+});
